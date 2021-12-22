90de2a6913f5c2228d125b725c988a83395664ab
Juan Frias <juandfrias@gmail.com>
2021-12-22 00:00:47 -0800

    GH-20 DB Schema update, register table

--- ironsigma/database/schema.sql
+++ ironsigma/database/schema.sql
@@ -12,3 +12,20 @@ CREATE TABLE `recurring` (
   PRIMARY KEY (`recur_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
+
+-- Register Table
+
+CREATE TABLE `register` (
+  `reg_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `date` DATE NOT NULL,
+  `payee` varchar(128) NOT NULL,
+  `memo` varchar(128) DEFAULT NULL,
+  `amount` decimal(8,2) NOT NULL,
+  `icon` varchar(64) DEFAULT NULL,
+  `color` varchar(64) DEFAULT NULL,
+  `recur_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`reg_id`),
+  KEY `reg_recur_fk` (`recur_id`),
+  CONSTRAINT `reg_recur_fk` FOREIGN KEY (`recur_id`)
+  REFERENCES `recurring` (`recur_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
